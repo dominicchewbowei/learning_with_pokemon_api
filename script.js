257df18461ef0fe1b83639384aeb66ffeffca299
dominicchew <dominicchew@stigp.com>
2025-11-22 10:50:25 +0800

    pokemon list side bar enhancement: addition of html id

--- script.js
+++ script.js
@@ -10,12 +10,15 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0", requestOptions)
   .then((result) => {
         data = JSON.parse(result);
         console.log(data.results.length);
-        for (let i = 0; i < data.results.length; i++)     // print 10 first
-
+        const divElement = document.getElementById("pokemon-list");
+        for (let i = 0; i < data.results.length; i++)
         {
             console.log(`${data.results.name}`);
-            document.getElementById("pokemon-list").innerHTML += 
-                `#${i} \t ${data.results[i].name}<br>`;
+            const currentPokemonNode = document.createElement('a');
+            currentPokemonNode.textContent =`#${String(i+1).padStart(4, '0')} \t ${data.results[i].name}`;
+            currentPokemonNode.id = `${data.results[i].name}`;
+            divElement.appendChild(currentPokemonNode);
+            divElement.append(document.createElement('br'));
         }
   })
   .catch((error) => console.error(error));
